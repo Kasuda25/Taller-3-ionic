e5dae2ac6a2eb2ee6416d56f3d01a85c92cd2cd2
Anisley Povea <poveanis@gmail.com>
2024-11-17 18:00:58 -0500

    fix error

--- src/app/Pages/vaccine/vaccine.module.ts
+++ src/app/Pages/vaccine/vaccine.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { VaccinePageRoutingModule } from './vaccine-routing.module';
+import { VaccinePageRoutingModule } from 'src/app/Pages/vaccine/vaccine-routing.module';
 import { VaccinePage } from './vaccine.page';
 import { VaccineComponentComponent } from 'src/app/vaccine-component/vaccine-component.component';
+
 
 @NgModule({
   imports: [
