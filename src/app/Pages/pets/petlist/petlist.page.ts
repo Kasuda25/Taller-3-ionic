4c0f635e1b11d2911d6c74f5814a957dc56a0c91
Anisley Povea <poveanis@gmail.com>
2024-11-18 16:19:36 -0500

    Add vaccines, edit delete.

--- src/app/Pages/pets/petlist/petlist.page.ts
+++ src/app/Pages/pets/petlist/petlist.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from 'src/app/shared/services/database/database.service';
 import { Pet } from '../pets/pet.model';
+import { NavController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-petlist',
@@ -10,7 +13,8 @@ import { Pet } from '../pets/pet.model';
 export class PetlistPage implements OnInit {
   pets: Pet[] = [];
 
-  constructor(private petService: DatabaseService) { }
+
+  constructor(private petService: DatabaseService, private navCtrl: NavController) { }
 
   ngOnInit() {
     this.petService.getPets().subscribe((pets) => {
@@ -21,4 +25,10 @@ export class PetlistPage implements OnInit {
   onEdit(pet: Pet) {
     console.log('Editar mascota', pet);
   }
+
+  onViewVaccines(event: Event, pet: Pet) {
+    event.stopPropagation();
+    this.navCtrl.navigateForward(`/vaccine/${pet.id}`);
+  }
+ 
 }
